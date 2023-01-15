9c971e8aaedcc9f6170a101cfda15b39f70ea903
DESKTOP-TILHQ4P\user <lmhnote@naver.com>
2023-01-15 19:49:37 +0900

    처음 올림

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const { Post } = require('./post')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const port = process.env.PORT
+const server = app.listen(port, () => {
+    console.log(`${port} running`)
+});
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(() => console.log(err))
+
+
+app.get('/', (req, res) => res.send('안녕하세요'))
+
+app.post('/api/users/post', (req, res) => {
+    const posting = new Post(req.body)
+
+    posting.save((err, postInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({success : true})
+    })
+})
+
+app.get('/api/users/hh', (req, res) => {
+    Post.find((err, data) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json(data)
+    })
+})
